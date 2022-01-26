26a6212032fa686fe31d3e41cad5a064f52714cd
xylonx <x956200361@gmail.com>
2022-01-26 15:14:31 +0800

    :ambulance: (env) load env must contains REACT_APP_ prefix

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,7 +1,7 @@
 // const { BASE_URL } = process.env;
 
 // eslint-disable-next-line no-undef
-const baseURL = process.env.API_URL;
+const baseURL = process.env.REACT_APP_API_URL;
 
 export default {
     BaseURL: baseURL,
