3185f77f162d51bffec6e543f71a092ed4844432
xylonx <x956200361@gmail.com>
2022-01-26 15:08:13 +0800

    :wrench: (BaseURL) load baseURL by API_URL env directly

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,19 +1,7 @@
 // const { BASE_URL } = process.env;
 
-let baseURL;
 // eslint-disable-next-line no-undef
-switch (process.env.NODE_ENV) {
-    case "development":
-        baseURL = "http://127.0.0.1:5050/api/v1";
-        break;
-
-    case "production":
-        baseURL = "https://back.icc.xylonx.com/api/v1";
-        break;
-
-    case "test":
-        baseURL = "https://back.preview.icc.xylonx.com/api/v1";
-}
+const baseURL = process.env.API_URL;
 
 export default {
     BaseURL: baseURL,
